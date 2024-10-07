7f29d1055c07081288b7484744d754996cc87f6c
amar <amar31661@gmail.com>
2024-10-07 18:31:05 +0530

    created dotenv3

--- dp_operations.py
+++ dp_operations.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:nowisgood@localhost:5432/blog_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 
 class Post(db.Model):
